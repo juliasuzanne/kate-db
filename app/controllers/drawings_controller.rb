d1c4c4e4f9846671c45f72597561202725a49dfa
Julia Grimes <julia.s.grimes@gmail.com>
2023-06-28 15:18:20 -0400

    Added create method for drawings

--- app/controllers/drawings_controller.rb
+++ app/controllers/drawings_controller.rb
@@ -1,2 +1,16 @@
 class DrawingsController < ApplicationController
+  def index
+    drawings = Drawing.all
+    render json: drawings.as_json
+  end
+
+  def create
+    drawing = Drawing.create(
+      name: params[:name],
+      tags: params[:tags],
+      description: params[:description],
+      url: params[:url],
+    )
+    render json: drawing.as_json
+  end
 end
